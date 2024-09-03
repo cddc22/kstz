619dd5cb0cde78d086805996a3adec2c119ae8c3
d <cddc22@users.noreply.github.com>
2024-09-03 15:25:58 +0800

    Update merged2upload.py

--- subscribe/merged2upload.py
+++ subscribe/merged2upload.py
@@ -1,9 +1,12 @@
 import requests
-import base64
 import os
 import argparse
-import getpass
 
+# 异或加密函数
+def xor_encrypt_decrypt(data, key):
+    return ''.join(chr(ord(char) ^ key) for char in data)
+
+# 上传到 Gist 的函数
 def upload_to_gist(content, gist_id, github_token, filename):
     url = f"https://api.github.com/gists/{gist_id}"
     headers = {
@@ -29,8 +32,11 @@ def main():
     parser.add_argument('github_token', type=str, help='Your GitHub Token')
     args = parser.parse_args()
 
-    # Encrypt the GitHub token and convert it to base64
-    github_token = base64.b64encode(args.github_token.encode()).decode()
+    # 定义密钥
+    key = 0x55  # 这是一个示例密钥，你可以使用任何整数作为密钥
+
+    # 对 GitHub 令牌进行异或加密
+    github_token_encrypted = xor_encrypt_decrypt(args.github_token, key)
 
     current_dir = os.path.dirname(__file__)
     parent_dir = os.path.dirname(current_dir)
@@ -39,10 +45,12 @@ def main():
     with open(file_path, 'r') as file:
         content = file.read()
 
-    # Upload the content to the Gist
+    # 上传内容到 Gist
     gist_id = '712fc9f08cd794ca28352f7df2745cd5'
-    filename = 'clash.yaml'  # Specify the desired filename
-    upload_to_gist(content, gist_id, github_token, filename)
+    filename = 'clash.yaml'  # 指定所需的文件名
+    # 在上传之前，需要将令牌解密
+    github_token_decrypted = xor_encrypt_decrypt(github_token_encrypted, key)
+    upload_to_gist(content, gist_id, github_token_decrypted, filename)
 
 if __name__ == "__main__":
     main()
